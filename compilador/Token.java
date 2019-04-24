e87058ccf64cbd16e9e2b7368aa3fdfa62d4eb8e
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2019-04-24 10:55:38 -0300

    Adicionado linha do token

--- compilador/Token.java
+++ compilador/Token.java
@@ -4,20 +4,24 @@ class Token {
   public enum Tipo { ATRIB, OPR_AR, DELIM, ID, NUM, ERRO, FIM }
 
   final public static Token TOKEN_FINAL = new Token(Tipo.FIM, "fim");
+  
+  final public int linha;
   final public Tipo tipo;
   final public String valor;
 
-  Token(Tipo tipo) {
+  Token(int linha, Tipo tipo) {
+    this.linha = linha;
     this.tipo = tipo;
     this.valor = null;
   }
 
-  Token(Tipo tipo, String valor) {
+  Token(int linha, Tipo tipo, String valor) {
+    this.linha = linha;
     this.tipo = tipo;
     this.valor = valor;
   }
 
   public String toString() {
-    return "(" + tipo.name() + (valor == null ? "" : ", '" + valor + "'") + ")";
+    return "(" + linha + ": " + tipo.name() + (valor == null ? "" : ", '" + valor + "'") + ")";
   }
 }
